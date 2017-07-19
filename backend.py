a89d050c8e23f3526941db53ab83d53b58cc5c58
SUPERETDUPER <machst@live.ca>
2017-07-19 14:11:04 -0400

    Implemented add row method Removed unused index to column letter helper method

--- backend.py
+++ backend.py
@@ -1,3 +1,4 @@
+import collections
 import datetime
 import os.path
 
@@ -13,11 +14,11 @@ typeColumn = "Type"
 subtypeColumn = "Sub-type"
 weightColumn = "Weight"
 
-columns = {idColumn: 0,
-           timeColumn: 1,
-           typeColumn: 2,
-           subtypeColumn: 3,
-           weightColumn: 4}
+columns = collections.OrderedDict([(idColumn, 0),
+                                   (timeColumn, 1),
+                                   (typeColumn, 2),
+                                   (subtypeColumn, 3),
+                                   (weightColumn, 4)])
 
 
 class Database:
@@ -44,7 +45,7 @@ class Database:
                     empty = False
 
             if empty:  # If empty tracker is still True after looping through all the row's cells, this line is empty
-                self.lastRow = index
+                self.lastRow = index + 1
                 break
 
             if self.last_id < row[columns[idColumn]].value:  # Update to get latest id assigned
@@ -58,12 +59,21 @@ class Database:
     def save(self):
         self.workbook.save(filename=db_file_path)
 
-    def get_info(self):
-        print("Last row : " + str(self.lastRow))
-        print("Last id :" + str(self.last_id))
-
     def add_item(self, row):
-        return 0
+        empty_row = self.ws[self.lastRow + 1]
+
+        for index, cell in enumerate(columns):
+
+            value = row.iter_row()[index]
+
+            if value is not None:
+                empty_row[index].value = row.iter_row()[index]
+
+            if index == 0:
+                empty_row[index].value = self.last_id + 1
+                self.last_id += 1
+
+        self.lastRow += 1
 
     def remove_item(self, batch_number):
         return True
@@ -92,10 +102,9 @@ class Row:
         return self.row[index]
 
 
-def get_letter(index):
-    return chr(ord("A") + index)
-
-
 globalvar.database = Database()
+
+globalvar.database.add_item(Row("Chicken", "Wings", 50))
+globalvar.database.add_item(Row("Chicken", "Wings", 3))
+
 globalvar.database.save()
-globalvar.database.get_info()
